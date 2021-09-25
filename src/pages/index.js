0f15461ae241391c13960f225d79e8ad9f97aed4
AhmedTajalsir <ah.alkhier2@gmail.com>
2021-09-26 03:29:27 +0530

    app build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,36 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({products}) {
+  // console.log(products);
   return (
-    <div>
+    <div className='bg-gray-100 '>
       <Head>
-        <title>App Name</title>
+        <title>Amazon</title>
       </Head>
-
-      {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
-      <center className="grid place-items-center mt-10">
-       
-        <h3 className="font-bold">
-          Tamplate using Next.js / Tailwind CSS / Redux / Firebase / NextAuth
-        </h3>
-       
+      
+      {/* Header */}
+      <Header />
+      <main className='max-w-screen-2xl mx-auto '>
+        {/* Banner */}
+        <Banner />
+        {/* Product Feed */}
+        <ProductFeed  products={products}/>
         
-
-        <p className="mt-24">Built with 💙 by Ahmed Tajalsir </p>
-      </center>
-      {/* ---- ---- */}
+      </main>
     </div>
   );
+}
+
+export async function getServerSideProps (context){
+//GET => https://fakestoreapi.com/products/
+ const products = await fetch('https://fakestoreapi.com/products/')
+ .then((res)=> res.json())
+ return { 
+   props : {
+    products,
+   },
+  };
 }
